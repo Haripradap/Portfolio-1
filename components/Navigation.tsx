30977eaa3ac224f57f6282fa3094a45f89dd7fab
Haripradap <140403832+Haripradap@users.noreply.github.com>
2024-12-08 16:27:16 +0530

    changed error in deployment

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -25,19 +25,19 @@ const Navigation = () => {
 
       return () => clearTimeout(timeout);
     }
-  }, [isRouting]);
+  }, [isRouting, path]);
+
   return (
     <div
       style={{ left: "20%" }}
       className="absolute z-[50] -bottom-20 w-[50%] md:w-[20%] max-h-[150px] rounded-full flex justify-between items-center border bg-black border-white px-4 py-7"
     >
-
       {isRouting && <Transition />}
       {NavLinks.map((nav) => (
         <Link key={nav.name} href={nav.link} className="mb-16 pl-4 min-w-[20%]">
           <nav.icon
             className={`w-[24px] h-[24px] ${
-              path === nav.name ? "text-purple-800" : "text-white"
+              path === nav.link ? "text-purple-800" : "text-white"
             }`}
           />
         </Link>
--- components/Transition.tsx
+++ components/Transition.tsx
@@ -3,18 +3,9 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const TransitionVariants = {
-  initial: {
-    y: "100%",
-    height: "100%",
-  },
-  animate: {
-    y: "0%",
-    height: "0%",
-  },
-  exit: {
-    y: ["0%", "100%"],
-    height: ["0%", "100%"],
-  },
+  initial: { y: "100%", height: "100%" },
+  animate: { y: "0%", height: "0%" },
+  exit: { y: ["0%", "100%"], height: ["0%", "100%"] },
 };
 
 const Transition = () => {
