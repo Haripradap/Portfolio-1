dd0a10de2a3f586a0b8d512a730bb7117c3c0f6b
Haripradap <140403832+Haripradap@users.noreply.github.com>
2024-12-08 17:21:16 +0530

    Finished

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -6,39 +6,25 @@ import React, { useEffect, useState } from "react";
 import Transition from "./Transition";
 
 const Navigation = () => {
-  const [isRouting, setisRouting] = useState(false);
+  const [isRouting, setIsRouting] = useState(false);
   const path = usePathname();
-  const [prevPath, setPrevPath] = useState("/");
 
   useEffect(() => {
-    if (prevPath !== path) {
-      setisRouting(true);
-    }
-  }, [path, prevPath]);
+    setIsRouting(true);
+    const timeout = setTimeout(() => {
+      setIsRouting(false);
+    }, 1200);
 
-  useEffect(() => {
-    if (isRouting) {
-      setPrevPath(path);
-      const timeout = setTimeout(() => {
-        setisRouting(false);
-      }, 1200);
-
-      return () => clearTimeout(timeout);
-    }
-  }, [isRouting, path]);
+    return () => clearTimeout(timeout);
+  }, [path]);
 
   return (
-    <div
-      style={{ left: "20%" }}
-      className="absolute z-[50] -bottom-20 w-[50%] md:w-[20%] max-h-[150px] rounded-full flex justify-between items-center border bg-black border-white px-4 py-7"
-    >
+    <div className="absolute z-[50] -bottom-20 w-[50%] md:w-[20%] max-h-[150px] rounded-full flex justify-between items-center border bg-black border-white px-4 py-7 left-[20%]">
       {isRouting && <Transition />}
       {NavLinks.map((nav) => (
         <Link key={nav.name} href={nav.link} className="mb-16 pl-4 min-w-[20%]">
           <nav.icon
-            className={`w-[24px] h-[24px] ${
-              path === nav.link ? "text-purple-800" : "text-white"
-            }`}
+            className={`w-[24px] h-[24px] ${path === nav.link ? "text-purple-800" : "text-white"}`}
           />
         </Link>
       ))}
--- components/Transition.tsx
+++ components/Transition.tsx
@@ -2,16 +2,19 @@
 import React from "react";
 import { motion } from "framer-motion";
 
+
+
 const TransitionVariants = {
   initial: { y: "100%", height: "100%" },
   animate: { y: "0%", height: "0%" },
   exit: { y: ["0%", "100%"], height: ["0%", "100%"] },
 };
-
 const Transition = () => {
   return (
     <div>
       <motion.div
+     
+
         className="fixed right-0 h-screen w-screen bottom-full z-[30] bg-[#2e2257]"
         variants={TransitionVariants}
         initial="initial"
